29edc457959750c6fe1b1491448c639f4f5cc2fb
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2021-02-07 20:56:17 +0800

    Add task to gulpfile : PUG

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,17 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+
+const routes = {
+    pug : {
+        watch : "src/**/*.pug",
+        src : "src/*.pug",
+        dest : "build"
+    }
+}
+
+const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest))
+
+
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([assets])
