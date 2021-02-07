89fb95f717c9b4372bcebcbf8ae16046e33c22f7
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2021-02-07 21:27:28 +0800

    Add tasks to gulpfile(3) : gulp-sass node-sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,18 +2,39 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import img from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require('node-sass');
 
 const routes = {
     pug : {
         watch : "src/**/*.pug",
         src : "src/*.pug",
         dest : "build"
+    },
+    img : {
+        src : "src/img/*",
+        dest : "build/img"
+    },
+    scss : {
+        watch: "src/scss/**/*.scss",
+        src : "src/scss/style.scss",
+        dest: "build/css"
     }
 }
 
 const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest))
 
-const watch = () => gulp.watch(routes.pug.watch, pug);
+const image = () => gulp.src(routes.img.src).pipe(img()).pipe(gulp.dest(routes.img.dest))
+
+const styles = () => gulp.src(routes.scss.src).pipe(sass().on('error', sass.logError)).pipe(gulp.dest(routes.scss.dest))
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, image);
+    gulp.watch(routes.scss.watch, styles)
+}
 
 const webserver = () => gulp.src("build").pipe(ws({
     livereload: true,
@@ -27,6 +48,6 @@ const prepare = gulp.series([clean])
 const live = gulp.parallel([webserver, watch])
 
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, image, styles]);
 
 export const dev = gulp.series([prepare,assets,live])
