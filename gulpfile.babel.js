5c3e7f80baf9c4652680b9b4c4dd470fad039579
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2021-02-07 21:05:39 +0800

    Add tasks to gulpfile(2) :  initializing destination folder, opening dev server, watching files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
     pug : {
@@ -11,7 +13,20 @@ const routes = {
 
 const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest))
 
+const watch = () => gulp.watch(routes.pug.watch, pug);
+
+const webserver = () => gulp.src("build").pipe(ws({
+    livereload: true,
+    open: true
+}))
+
+const clean = () => del(["build"])
+
+const prepare = gulp.series([clean])
+
+const live = gulp.parallel([webserver, watch])
+
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([assets])
+export const dev = gulp.series([prepare,assets,live])
